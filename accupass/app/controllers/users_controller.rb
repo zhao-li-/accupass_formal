e9f55441f24f63205cbe757e1f58aeb06d0810db
zhao-li- <1216186168@qq.com>
2014-05-03 09:57:52 +0800

    css

--- accupass/app/controllers/users_controller.rb
+++ accupass/app/controllers/users_controller.rb
@@ -150,7 +150,6 @@ class UsersController < ApplicationController
     messages = BidMessage.where(:bid_id => params[:bid_id],:activity_name=>params[:activity_name],:current_user => params[:current_user])
     @bid_messages = messages.paginate(page: params[:page],per_page: 10)
     price_static = messages.group_by{|message|message.price}.map { |key,value|{price: key,count: value.length}}
-    @page_price_static = price_static.paginate(page: params[:page],per_page: 10)
     winner = price_static.sort_by { |static|static[:price].to_i}.find{|static|static[:count] == 1}
     bid = Bid.where(:bid_id => params[:bid_id],:activity_name=>params[:activity_name],:current_user => params[:current_user])
     if bid[0][:status]=="start"
@@ -166,7 +165,6 @@ class UsersController < ApplicationController
   def price_static
     @page_index = params[:page] ||1
     messages = BidMessage.where(:bid_id => params[:bid_id],:activity_name=>params[:activity_name],:current_user => params[:current_user])
-    @bid_messages = messages.paginate(page: params[:page],per_page: 10)
     price_static = messages.group_by{|message|message.price}.map { |key,value|{price: key,count: value.length}}
     @page_price_static = price_static.paginate(page: params[:page],per_page: 10)
     winner = price_static.sort_by { |static|static[:price].to_i}.find{|static|static[:count] == 1}
